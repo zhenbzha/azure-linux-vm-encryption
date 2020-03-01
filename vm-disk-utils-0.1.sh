f07e5383b75de66f768996a0f8528af2dc70c0e9
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2020-03-01 13:05:03 +0200

    fix the bug that /etc/fstab has ext4 format

--- vm-disk-utils-0.1.sh
+++ vm-disk-utils-0.1.sh
@@ -178,7 +178,7 @@ add_to_fstab() {
     then
         echo "Not adding ${UUID} to fstab again (it's already there!)"
     else
-        LINE="UUID=\"${UUID}\"\t${MOUNTPOINT}\text4\t${MOUNT_OPTIONS}\t1 2"
+        LINE="UUID=\"${UUID}\"\t${MOUNTPOINT}\txfs\t${MOUNT_OPTIONS}\t1 2"
         echo -e "${LINE}" >> /etc/fstab
     fi
 }
